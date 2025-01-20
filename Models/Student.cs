c457231d458419c89e832f31867f566db081004a
venu soni <75362581+Venuso12@users.noreply.github.com>
2025-01-20 15:09:03 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstcoreApp.Models
+{
+    public class Student
+    {
+        [Key]
+
+        public int id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string lastName { get; set; }
+        public string email { get; set; }
+        
+
+    }
+}
---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstcoreApp.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-0H6GGHA;Initial Catalog=Core_db;User ID=sa;Password=123;Encrypt=False");
+        }
+
+
+
+
+
+    }
+}
